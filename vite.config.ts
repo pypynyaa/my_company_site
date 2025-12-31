ccd91ab35b7da3a0f1d96fc8414add2a934c3e6c
pypynyaa <nikita662006@bk.ru>
2025-12-31 06:46:53 +0300

    new keys

--- vite.config.ts
+++ vite.config.ts
@@ -1,18 +1,18 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
 import path from "path";
-import { componentTagger } from "lovable-tagger";
 
-// https://vitejs.dev/config/
-export default defineConfig(({ mode }) => ({
+export default defineConfig({
+  // ОБЯЗАТЕЛЬНО: добавь это (имя твоего репозитория в кавычках)
+  base: "/my_company_site/", 
   server: {
-    host: "::",
+    host: "0.0.0.0",
     port: 8080,
   },
-  plugins: [react(), mode === "development" && componentTagger()].filter(Boolean),
+  plugins: [react()],
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
     },
   },
-}));
+});
